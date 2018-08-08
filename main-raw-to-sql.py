b81b6b39b271a4850b6d52df9810b5b0b0e89f96
Eduard Chai <eduard.chai@salestock.id>
2018-08-09 02:57:53 +0800

    remove cred

--- main-raw-to-sql.py
+++ main-raw-to-sql.py
@@ -15,15 +15,15 @@ from unemployed_citizens_annual import unemployed_citizens_annual
 
 if __name__ == "__main__":
     # Fill this with MS SQL Server address
-    server = '35.240.235.125'
-    database = 'ke5106'
-    username = 'admin'
-    password = '12345678'
+    server = ''
+    database = ''
+    username = ''
+    password = ''
     
     # ODBC Driver could be different (depends on OS)
     # Code example can be found here: https://docs.microsoft.com/en-us/sql/connect/python/python-driver-for-sql-server?view=sql-server-2017
-    # driver='{ODBC Driver 13 for SQL Server}' # For Windows
-    driver='/usr/local/lib/libmsodbcsql.13.dylib' # For MacOS
+    driver='{ODBC Driver 13 for SQL Server}' # For Windows
+    # driver='/usr/local/lib/libmsodbcsql.13.dylib' # For MacOS
 
     cause_of_death.main(server, database, username, password, driver)
     cpi_level.main(server, database, username, password, driver)
--- main-sql-to-bq.py
+++ main-sql-to-bq.py
@@ -2,17 +2,17 @@ from fact_tables import fact_employment,fact_happiness,fact_living_cost,fact_mar
 
 if __name__ == "__main__":
     # Fill this with MS SQL Server address
-    server = '35.240.235.125'
-    database = 'ke5106'
-    username = 'admin'
-    password = '12345678'
+    server = ''
+    database = ''
+    username = ''
+    password = ''
+    bq_dataset = ''
     
     # ODBC Driver could be different (depends on OS)
     # Code example can be found here: https://docs.microsoft.com/en-us/sql/connect/python/python-driver-for-sql-server?view=sql-server-2017
     # driver='{ODBC Driver 13 for SQL Server}' # For Windows
     driver='/usr/local/lib/libmsodbcsql.13.dylib' # For MacOS
 
-    bq_dataset = "ca_1"
     fact_employment.main(server, database, username, password, driver, bq_dataset, "fact_employment")
     fact_happiness.main(server, database, username, password, driver, bq_dataset, "fact_happiness")
     fact_living_cost.main(server, database, username, password, driver, bq_dataset, "fact_living_cost")
