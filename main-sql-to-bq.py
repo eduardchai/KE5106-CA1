1df33420360f4f69aa07549fcbd0ea41a0d6d5c5
Eduard Chai <eduard.chai@salestock.id>
2018-08-12 01:27:20 +0800

    update variable name

--- main-sql-to-bq.py
+++ main-sql-to-bq.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     database = ''
     username = ''
     password = ''
-    bq_dataset = ''
+    bigquery_dataset = ''
     
     # ODBC Driver could be different (depends on OS)
     # Code example can be found here: https://docs.microsoft.com/en-us/sql/connect/python/python-driver-for-sql-server?view=sql-server-2017
     driver='{ODBC Driver 13 for SQL Server}' # For Windows
     # driver='/usr/local/lib/libmsodbcsql.13.dylib' # For MacOS
 
-    fact_employment.main(server, database, username, password, driver, bq_dataset, "fact_employment")
-    fact_happiness.main(server, database, username, password, driver, bq_dataset, "fact_happiness")
-    fact_living_cost.main(server, database, username, password, driver, bq_dataset, "fact_living_cost")
-    fact_marital_status.main(server, database, username, password, driver, bq_dataset, "fact_marital_status")
+    fact_employment.main(server, database, username, password, driver, bigquery_dataset, "fact_employment")
+    fact_happiness.main(server, database, username, password, driver, bigquery_dataset, "fact_happiness")
+    fact_living_cost.main(server, database, username, password, driver, bigquery_dataset, "fact_living_cost")
+    fact_marital_status.main(server, database, username, password, driver, bigquery_dataset, "fact_marital_status")
